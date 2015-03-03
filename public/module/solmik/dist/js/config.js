c2768860fbd3ee1e896746f19174822199ee2ef7
Michael Kox <git1@michael-kox.de>
2015-03-03 22:04:37 +0100

    filled dist folder

--- public/module/solmik/dist/js/config.js
+++ public/module/solmik/dist/js/config.js
@@ -1582,7 +1582,7 @@ define('solmi',[
 require.config({
 //  baseUrl: 'js/',
     paths: {
-        jquery: [ 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', 'libs/jquery/jquery-1.11.2.min' ],
+        jquery: [ 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min', 'libs/jquery/jquery-1.11.2.min' ],
         underscore: 'libs/underscore/underscore-min-1.7.0',
         backbone: 'libs/backbone/backbone-min-1.1.2',
 //    helpers: 'helpers',
@@ -1607,7 +1607,7 @@ require.config({
             deps: ["jquery", "underscore"],
             exports: "Backbone"
         }
-    },
+    }
 //    deps: ['solmi']
 
 });
